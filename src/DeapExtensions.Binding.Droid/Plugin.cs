0ac63f8584284afc6face9361a4bd020f5849937
Hugo Logmans <hlogmans@edb.nl>
2013-08-05 16:33:03 +0200

    Rebuild to plugin, but it is not registering quite well. Resolving that later.

---
+++ src/DeapExtensions.Binding.Droid/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Android.Views;
+using Cirrious.CrossCore;
+using Cirrious.CrossCore.IoC;
+using Cirrious.CrossCore.Plugins;
+using DeapExtensions.Binding.Droid.Views;
+
+namespace DeapExtensions.Binding.Droid
+{
+    public class Plugin : IMvxPlugin
+    {
+        public void Load()
+        {
+            Mvx.RegisterType(typeof(BindableGroupListAdapter), typeof(BindableGroupListAdapter));
+            Mvx.RegisterType(typeof(BindableExpandableListAdapter), typeof(BindableExpandableListAdapter));
+            Mvx.RegisterType(typeof(BindableGroupListView), typeof(BindableGroupListView));
+            Mvx.RegisterType(typeof(BindableExpandableListView), typeof(BindableExpandableListView));
+
+            //var viewCache = Mvx.Resolve<IMvxTypeCache<View>>();
+            //viewCache.AddAssembly(typeof(BindableExpandableListAdapter).Assembly);
+
+        }
+    }
+}
